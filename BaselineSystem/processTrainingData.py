bf61e853c49b92149b92842f26cf0802a9918349
Petter Asla <petter.asla@gmail.com>
2015-10-21 18:50:01 +0200

    adding some statistics regarding training data

--- BaselineSystem/processTrainingData.py
+++ BaselineSystem/processTrainingData.py
@@ -1,4 +1,12 @@
+import re
+
 def processData(doc_name):
+    """
+    Process the text file from semEval task 6 training data to list
+
+    :param doc_name:    String containing the document file name
+    :return:            A list with lists of data separated by id, topic,tweet and stance
+    """
     data = []
     f = open(doc_name,"r")
     line = f.readline()     #Firste line states id, year, title of the article
@@ -22,20 +30,134 @@ def processData(doc_name):
     f.close()
     return data
 
-# data = processData("semeval2016-task6-trainingdata.txt")
-    #
-    # print "Lenght of the data: " +str(len(data))
-    # print "Test sample from the topic: Atheism"
-    # print data[1]
-    # print
-    # print "Test sample from the topic: Climate Change is a Real Concern"
-    # print data[620]
-    # print
-    # print "Test sample from the topic: Feminist Movement"
-    # print data[920]
-    # print
-    # print "Test sample from the topic: Hillary Clinton"
-    # print data[1700]
-    # print
-    # print "Test sample from the topic: Legalization of Abortion"
-    # print data[2400]
+#data = processData("semeval2016-task6-trainingdata.txt")
+
+def getTopicData(topic):
+    """
+    Extracts the data from a given topic
+
+    :param topic:   A string with topic name [All, Atheism, Climate Change is a Real Concern,
+                    Feminist Movement, Hillary Clinton, Legalization of Abortion]
+    :return:        A list with information about that topic
+    """
+    data = processData("semeval2016-task6-trainingdata.txt")
+    if (topic == "All"):
+        return data
+    topicData = []
+    for i in range(len(data)):
+        if data[i][1] == topic:
+            topicData.append(data[i])
+    return topicData
+
+def getAllTweets(data_file="All"):
+    """
+    Extracts all the tweets from the processed data
+
+    :param data_file:   Either a list with data (from getTopicData(topic)) or the whole dataset
+    :return:            A list with all the tweets
+    """
+    if data_file == "All":
+        data = processData("semeval2016-task6-trainingdata.txt")
+    else:
+        data = data_file
+    tweets = []
+    for i in range(len(data)):
+        tweets.append(data[i][2].decode("ISO-8859-1"))
+    return tweets
+
+def getAllStances(data_file="All"):
+    """
+    Extracts all the stances from the processed data
+
+    :param data_file:   Either a list with data (from getTopicData(topic)) or the whole dataset 'All'
+    :return stance:     A list with all the stances
+    """
+    if data_file == "All":
+        data = processData("semeval2016-task6-trainingdata.txt")
+    else:
+        data = data_file
+    stance = []
+    for i in range(len(data)):
+        stance.append(data[i][3])
+    return stance
+
+def getAllHashtags(tweet_list):
+    """
+    Extracts all the hashtags from the tweets
+
+    :param tweet_list:   A list with lists of all the tweets
+    :return hashtags:    A list with lists of the hashtags for each tweet
+    """
+    data = tweet_list
+    hashtags = []
+    for line in data:
+        hash = []
+        hashStartIndex = line.find("#")
+        stopIndex = line.find("#SemST")
+        while hashStartIndex < stopIndex:
+            hashStopIndex = line.find(" ", hashStartIndex)
+            hash.append(line[hashStartIndex:hashStopIndex])
+            hashStartIndex = line.find("#", hashStartIndex+1)
+        hashtags.append(hash)
+    return hashtags
+
+
+def decryptHashtags(hashes):
+    """
+    Takes a single list of hashtags, removes the hashes and tries to split the tags into words
+
+    :param hashes:      A list of hashes
+    :return wordList:   A list of words with no duplicates nor hashtags
+    """
+    wordList = set()
+    for tag in hashes:
+        if tag[1:].isupper() or tag[1:].islower():
+            wordList.add(tag[1:])
+        else:
+            words = re.findall('[A-Z][^A-Z]*',tag[1:])
+            for word in words:
+                wordList.add(word)
+
+    return list(wordList)
+
+def decryptAllHashtags(hashtag_list):
+    """
+    Takes a list with lists of hashtags and decrypts into words using decryptHashtags(hashes).
+
+    :param hashtag_list:    A list with lists of hashtags
+    :return wordList:       A list of words without hashtags and no duplicates
+    """
+    wordList = set()
+    for hashes in hashtag_list:
+        words = decryptHashtags(hashes)
+        for word in words:
+            wordList.add(word)
+    return list(wordList)
+
+
+# Example use
+TOPIC = "All"
+TOPIC2 = "Climate Change is a Real Concern"
+TOPIC1 = "Atheism"
+TOPIC3 = "Feminist Movement"
+TOPIC4 = "Hillary Clinton"
+TOPIC5 = "Legalization of Abortion"
+
+# Get all tweets and stances
+#allTweet = getAllTweets(getTopicData(TOPIC))
+#allStance = getAllStances(getTopicData(TOPIC5))
+
+#Get all the tweets from Climate change
+#tweets = getAllTweets(getTopicData(TOPIC2))
+
+#Get all the stance from Climate change
+#stance = getAllStances(getTopicData(TOPC2))
+
+#Get all the hashtags from tweets (within a topic)
+#hashtags = getAllHashtags(getAllTweets(getTopicData(TOPIC5)))
+#hashtags_in_one_list = [hash for hashes in hashtags for hash in hashes]
+
+#Decrypt all the hashtags from the topic: Climate Change is a real consern
+#words = decryptAllHashtags(hashtags)
+
+

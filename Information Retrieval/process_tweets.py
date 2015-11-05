e339fa1f2256ac41ed06fa55a648eeec9ff16156
Henrik Bøhler <henrik.boehler@gmail.com>
2015-11-05 11:30:06 +0100

    Added method to remove duplicate tweets (identical texts).

--- Information Retrieval/process_tweets.py
+++ Information Retrieval/process_tweets.py
@@ -9,6 +9,31 @@ def readTweets(filename):
             #print tweet['text'] + "\n"
             #print "##################################################################################################"
 
+    f.close()
     return tweets
 
+def removeDuplicates(filename):
+    with open(filename + '.json') as f:
+        # load json objects to dictionaries
+        jsons = map(json.loads, f)
+
+    uniques = {x['text']: x for x in jsons}
+
+    print uniques.values()
+
+    # write to new json file
+    with open(filename + '_clean.json' ,'w') as nf:
+        for js in uniques.values():
+            nf.write(json.dumps(js))
+            nf.write('\n')
+
+    f.close()
+    nf.close()
+    print len(readTweets(filename))
+    print len(readTweets(filename+'_clean'))
+
 #print readTweets('stream__climate')
+#print removeDuplicates('stream__chemtrails')
+#print removeDuplicates('stream__climate')
+#print removeDuplicates('stream__climatechange')
+#print removeDuplicates('stream__environment')

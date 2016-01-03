7bc8154bc68104cc65770c737dbf880e5448d2bb
Henrik Bøhler <henrik.boehler@gmail.com>
2016-01-03 13:20:11 -0800

    Some adjustments to label propagation

--- Information Retrieval/label_propagation.py
+++ Information Retrieval/label_propagation.py
@@ -12,6 +12,7 @@ def propagateLabels(dataset, filename):
 
     # Load test and training data
     labeled_data = ptd.getTopicData(dataset)
+    labeled_data = labeled_data[0:len(labeled_data)/4]
 
     labeled_tweets = ptd.getAllTweets(labeled_data)
     labeled_tweets_targets = ptd.getAllStances(labeled_data)
@@ -26,8 +27,6 @@ def propagateLabels(dataset, filename):
                                  ngram_range = (1,1),       # N-gram: (1,1) = unigram, (2,2) = bigram
                                  stop_words = "english")    # Built-in list of english stop words.)
     all_tweets_features = vectorizer.fit_transform(all_tweets)
-    #print all_tweets_features
-
 
     # Set number of desired labeled points
     n_total_samples = len(all_tweets)
@@ -35,25 +34,37 @@ def propagateLabels(dataset, filename):
 
     # Remove labels
     unlabeled_indices = np.arange(n_total_samples)[n_labeled_points:]
-    #print unlabeled_indices
-
 
     # Propagate labels
-    lp_model = label_propagation.LabelSpreading(gamma=0.25, max_iter=5)
-
-    #all_tweets = np.array([all_tweets]).T
-
+    lp_model = label_propagation.LabelSpreading(kernel='rbf', gamma=0.17, n_neighbors=7, alpha=0.2, max_iter=50, tol=0.01)
+    #lp_model = label_propagation.LabelSpreading(gamma=0.25, max_iter=5)
 
     lp_model.fit(all_tweets_features.toarray(), all_labels)
 
     predicted_labels = lp_model.transduction_[unlabeled_indices]
 
+    sum = 0
+    table = (all_labels==lp_model.transduction_)
+    for i in table:
+        if i == True:
+            sum += 1
 
-    #print (all_labels==lp_model.transduction_).all()
-    #print all_labels
-    #print lp_model.transduction_
-    #print np.count_nonzero(lp_model.transduction_ == 0)
-    #print lp_model.transduction_[530]
-    return lp_model.transduction_, all_tweets
+    print sum
+    print len(labeled_data)
 
-#print propagateLabels()
+    print lp_model.transduction_[:len(labeled_data)]
+    print lp_model.transduction_[len(labeled_data):]
+
+    predictions = [-1 for i in range(len(predicted_labels))]
+    for i in range(len(predicted_labels)):
+        predictions[i] = [predicted_labels[i], unlabeled_tweets[i]]
+
+    return predictions
+    #return lp_model.transduction_, all_tweets
+
+
+
+## USE THIS FOR TESTING
+results = propagateLabels("Climate Change is a Real Concern", "stream__climate_clean")
+#for result in results:
+#    print result

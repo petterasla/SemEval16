3f32166dd28498d907485a24f30463030dc99931
Petter Asla <petter.asla@gmail.com>
2016-01-04 20:05:53 +0100

    Added a logg so that we can write what we have done/tried

--- Information Retrieval/process_tweets.py
+++ Information Retrieval/process_tweets.py
@@ -12,6 +12,22 @@ def readTweets(filename):
     f.close()
     return tweets
 
+def readTweetsAndIDs(filename):
+    tweets = []
+    with open(filename + '.json') as f:
+        for line in f:
+            tweet = json.loads(line)
+            info = []
+            info.append(tweet['id'])
+            info.append(tweet['text'])
+            tweets.append(info)
+            #print tweet['text'] + "\n"
+            #print "##################################################################################################"
+
+    f.close()
+    return tweets
+
+
 def removeDuplicates(filename):
     with open(filename + '.json') as f:
         # load json objects to dictionaries
@@ -35,3 +51,21 @@ def removeDuplicates(filename):
 #removeDuplicates('stream__climate')
 #removeDuplicates('stream__climatechange')
 #removeDuplicates('stream__environment')
+
+
+# Tweets with id that are 'against' in: stream__cliatechange_clean.json:
+
+# [662022121904840704, 662020009183551488, 662016401067155456, 662020009783373824, 662016718303195136]
+
+# Tweets with id that are 'favor' in: stream__cliatechange_clean.json:
+
+# [662015471261245441, 662015310367752192, 662018031292256256, 662018963916722176, 662015805635325952, 662020824115896320, 662022433453572097 -
+"""
+tweets = readTweetsAndIDs("stream__climatechange_clean")
+for t in range(130,150):
+    try:
+        print tweets[t][0]
+        print tweets[t][1]
+    except:
+        print "error"
+        """

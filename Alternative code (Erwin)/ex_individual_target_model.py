8aa02b249135bd9bcb480fad359b5f7a55318f6c
Henrik Bøhler <henrik.boehler@gmail.com>
2016-02-04 17:03:04 +0100

    Cleaning up code.

--- Alternative code (Erwin)/ex_individual_target_model.py
+++ Alternative code (Erwin)/ex_individual_target_model.py
@@ -1,43 +1,47 @@
 import os
+import sys
+sys.path.append(os.path.abspath(__file__ + "/../../"))
 
 from glob import glob
 
 import pandas as pd
 
-from sklearn.pipeline import Pipeline, FeatureUnion, make_pipeline, make_union
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
-from sklearn.svm import SVC, LinearSVC
+from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.cross_validation import cross_val_predict, StratifiedKFold
 from sklearn.metrics import fbeta_score
-from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import VotingClassifier
-from sklearn.preprocessing import Normalizer
 
 from glove_transformer import GloveVectorizer
 
-import writePredictionsToFile as write
+import BaselineSystem.writePredictionsToFile as write
 from numpy import mean
-import processTrainingData as ptd
 
+
+# *****     SETTINGS     *****
 use_writeToFile_climate = 0
 use_writeToFile_all = 1
+use_threshold = 0
 
-
+# *****     LOAD DATA     *****
 test_data = pd.read_csv(open('SemEval2016-Task6-subtaskA-testdata.txt'), '\t', index_col=0)
 #test_data = test_data[test_data.Target == 'Climate Change is a Real Concern']
 
 original_data = pd.read_csv(open('semeval2016-task6-trainingdata.txt'), '\t', index_col=0)
 targets = list(original_data.Target.unique())
 
+# *****     PREPARE VARIABLES     *****
 best_model_score_list = []
 best_model_list = []
 best_weights = [[2,1,3], [3,1,3], [2,2,3],[2,1,3], [3,1,2]]
 i = 0
 glove_fnames = glob('*.pkl')
 glove_ids = [fname.split('/')[-1].split('_')[0] for fname in glove_fnames]
+
+# *****     BUILDING MODEL FOR EACH TARGET     *****
 for target in targets:
     target_data = original_data[original_data.Target == target]
     print 80 * "="
@@ -49,12 +53,12 @@ for target in targets:
     best_model_score = 0.0
     temp_list = []
 
+    # *****     FINDING BEST VECTOR SPACE     *****
     for fname, glove_id in zip(glove_fnames, glove_ids):
         print 80 * '='
         print 'GLOVE VECTORS:', glove_id
         print 80 * '='
 
-        glove_fname = 'glove/semeval2016-task6-trainingdata_climate_glove.twitter.27B.{}d.pkl'
         glove_vecs = pd.read_pickle(fname)
 
         glove_clf = Pipeline([('vect', GloveVectorizer(glove_vecs)),
@@ -77,6 +81,7 @@ for target in targets:
                                                       ngram_range=(2, 2))),
                              ('clf', MultinomialNB())])
 
+        # Creating the voting classifier which will be used
         vot_clf = VotingClassifier(estimators=[('char', char_clf),
                                                ('word', word_clf),
                                                ('glove', glove_clf),
@@ -93,19 +98,23 @@ for target in targets:
                               labels=['AGAINST', 'FAVOR'], average='macro')
         print 'macro-average of F-score(FAVOR) and F-score(AGAINST): {:.4f}\n'.format(macro_f)
 
+        # Storng best model so far
         if macro_f > best_model_score:
             best_model = vot_clf
             best_predictions = pred_stances
             best_model_score = macro_f
             temp_list.append(macro_f)
 
+    # Storing the best voting model
     best_model_score_list.append(max(temp_list))
     best_model_list.append(best_model)
     i +=1
 
+# *****     FINDING BEST "HARD" VOTING CLASSIFIER     *****
 climate_data = original_data[original_data.Target == "Climate Change is a Real Concern"]
 climate_best_f_score = 0.0
 
+# *****     FINDING BEST VECTORSPACE     *****
 for fname, glove_id in zip(glove_fnames, glove_ids):
     print 80 * '='
     print 'GLOVE VECTORS:', glove_id
@@ -113,7 +122,6 @@ for fname, glove_id in zip(glove_fnames, glove_ids):
     print "CLIMATE IS A CONCERN...=?=!?#=!?"
     print 80 * '='
 
-    glove_fname = 'glove/semeval2016-task6-trainingdata_climate_glove.twitter.27B.{}d.pkl'
     glove_vecs = pd.read_pickle(fname)
 
     cv = StratifiedKFold(climate_data.Stance, n_folds=5, shuffle=True, random_state=1)
@@ -137,6 +145,7 @@ for fname, glove_id in zip(glove_fnames, glove_ids):
                                                   ngram_range=(2, 2))),
                          ('clf', MultinomialNB())])
 
+    # Create voting classifier
     vot_clf = VotingClassifier(estimators=[('char', char_clf),
                                            ('word', word_clf),
                                            ('glove', glove_clf),
@@ -152,14 +161,17 @@ for fname, glove_id in zip(glove_fnames, glove_ids):
     macro_f = fbeta_score(climate_data.Stance, pred_stances, 1.0,
                           labels=['AGAINST', 'FAVOR'], average='macro')
     print 'macro-average of F-score(FAVOR) and F-score(AGAINST): {:.4f}\n'.format(macro_f)
+
+    # Save the best model so far
     if macro_f > climate_best_f_score:
         best_climate_model = vot_clf
         climate_best_f_score = macro_f
 
+# Printing the best scores an the mean
 print best_model_score_list
 print mean(best_model_score_list)
 
-use_threshold = 0
+# *****     WRITE PREDICTIONS TO FILE     *****
 if use_writeToFile_all:
     print "Writing gold and guesses to file on all of the targets..."
     unknown_list = []
@@ -231,3 +243,6 @@ if use_threshold:
     print "unknowns:"
     print unknown_list
     print "nr of against: " + str(counter_against)
+
+if not use_threshold:
+    os.system("perl /Users/Henrik/Documents/Datateknikk/Prosjektoppgave/SemEval16/BaselineSystem/eval.pl /Users/Henrik/Documents/Datateknikk/Prosjektoppgave/SemEval16/Results/all_gold.txt predict_all_with_individual_models.txt")
